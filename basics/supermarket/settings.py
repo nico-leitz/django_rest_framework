50790eea7a171645718e2e7abf1612be0c65505c
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-22 07:12:49 +0200

    feat(basics): add market app to settings.py and migrate models to db

--- basics/supermarket/settings.py
+++ basics/supermarket/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'market'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'supermarket.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'market.db',
     }
 }
 
