c40cffe94472490d14de7687bd831e850c05026e
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-23 16:27:33 +0200

    feat(basics): implement update and validate_location method for Market-Serializer

--- basics/market_app/api/serializiers.py
+++ basics/market_app/api/serializiers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from market_app.models import Market
 
+# Würde der Serializer meckern, wenn ich die länge überschreite?
+# Was macht der Serializer, was json.loads / json.dumps nicht macht und wieso nutzen wir den?
 class MarketSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
@@ -8,6 +10,26 @@ class MarketSerializer(serializers.Serializer):
     description = serializers.CharField()
     net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
 
-    def create(self, validated_data):
+    # Anhand der Instanz ist dem Serializer klar, welche methode (z.B. create oder update verwendet werden muss)
+    # bezieht sich das .self immer auf die jeweilige instanz (also immer eine?)
+
+    def create(self, validated_data): # Wird automatisch ausgeführt? (wenn ja wann??)
+        # Hat das was mit der "POST-Methode" aus den views zu tuhen??
         # Wir übergeben nicht das dictionary selbst die die keyword-dictonary arguments (kwargs)
-        return Market.objects.create(**validated_data) 
+        return Market.objects.create(**validated_data)
+
+    def update(self, instance, validated_data): # Wird automatisch ausgeführt? (wenn ja wann??)
+            # Hat das was mit der "PUT-Methode" aus den views zu tuhen??
+            instance.name = validated_data.get('name', instance.name)
+            instance.location = validated_data.get('location', instance.location)
+            instance.description = validated_data.get('description', instance.description)
+            instance.net_worth = validated_data.get('net_worth', instance.net_worth)
+            instance.save()
+            return instance
+
+    # Für einzelne Felder
+    def validate_location(self, value): # was ist der value?? (die location?)
+        if 'X' in value:
+            raise serializers.ValidationError('no X in location')
+        return value
+         
