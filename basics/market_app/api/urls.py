f5626a1d43dae6c19da22756bcafc21c1160ab1d
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-23 16:28:13 +0200

    feat(basics): implement market_single_view method for single market via primary_key (pk)

--- basics/market_app/api/urls.py
+++ basics/market_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import first_view
+from .views import first_view, markets_single_view
 
 urlpatterns = [
-  path('', first_view)
+  path('', first_view),
+  path('<int:pk>/',  markets_single_view)
 ]
