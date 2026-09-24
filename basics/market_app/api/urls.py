8853383e2dabf374b925cb3246b2d3881c45d119
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-24 20:48:44 +0200

    feat(basics): add seller single view endpoint

--- basics/market_app/api/urls.py
+++ basics/market_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import first_view, markets_single_view, seller_view
+from .views import first_view, markets_single_view, seller_view, single_seller_view
 
 urlpatterns = [
   path('market/', first_view),
   path('market/<int:pk>/',  markets_single_view),
-  path('seller/', seller_view)
+  path('seller/', seller_view),
+  path('seller/<int:pk>', single_seller_view)
 ]
