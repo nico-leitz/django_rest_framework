223287f4fb74fc415f7cb19c1566dfa6a6420450
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-25 19:45:39 +0200

    feat(basics): add name property to single_seller_view in urls.py

--- basics/market_app/api/urls.py
+++ basics/market_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import first_view, markets_single_view, seller_view, single_seller_view
 
+
 urlpatterns = [
   path('market/', first_view),
   path('market/<int:pk>/',  markets_single_view),
   path('seller/', seller_view),
-  path('seller/<int:pk>', single_seller_view)
+  path('seller/<int:pk>', single_seller_view, name='seller_single')
 ]
