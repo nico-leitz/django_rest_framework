2258de99cfcda586fe500cfdaa94795befbb63ac
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-26 10:58:16 +0200

    feat(basics): adjust api_view to class based view in urls

--- basics/market_app/api/urls.py
+++ basics/market_app/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import first_view, markets_single_view, seller_view, single_seller_view
+from .views import MarketsView, MarketsSingleView, seller_view, single_seller_view
 
 
 urlpatterns = [
-  path('market/', first_view),
-  path('market/<int:pk>/',  markets_single_view),
+  path('market/', MarketsView.as_view()),# class based views müssen immer mit 'as_view()' deklariert werden
+  path('market/<int:pk>/',  MarketsSingleView.as_view(), name='market-detail'),
   path('seller/', seller_view),
   path('seller/<int:pk>', single_seller_view, name='seller_single')
 ]
