7d0e4f5cc9b3f74606beeac512e6f4904b5dac81
nico-leitz <ndesk.ltrz@gmail.com>
2026-09-26 10:57:36 +0200

    feat(basics): learn about mixins_views und the simplicity in comparison to api_views

--- basics/market_app/api/views.py
+++ basics/market_app/api/views.py
@@ -3,49 +3,61 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializiers import MarketSerializer, SellerSerializer, MarketHyperlinkedSerializer
 from market_app.models import Market, Seller
+from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
-@api_view(['GET', 'POST']) 
-def first_view(request):
+# Bekommen wir hier eine HTML-View?? Was nutzt man das, bzw wo ist der unterschied zu den anderen??
+# Ist das hier die ListView für POST und GET??
+class MarketsView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+            return self.create(request, *args, **kwargs)
+
+
+class MarketsSingleView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin ,generics.GenericAPIView):
+
+    # Wird für das GenericAPIView benötigt? (Die zwei Zeilen)
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs) # retrieve, weil ein einzelne Instanz/Objekt??
+
+    def put(self, request, *args, **kwargs):
+            return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+                return self.destroy(request, *args, **kwargs)
+
+# @api_view(['GET', 'DELETE', 'PUT'])
+# def markets_single_view(request, pk):
     
-    if request.method == 'GET':
-        markets = Market.objects.all() 
-        # Hier nutzen wir die Vererbung
-        serializer = MarketHyperlinkedSerializer(markets, many=True, context={'request': request}) 
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = MarketSerializer(data=request.data, many=True, context={'request': request})
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-        
+#     if request.method == 'GET':
+#         market = Market.objects.get(pk=pk) 
+#         serializer = MarketSerializer(market, many=False, context={'request': request}) 
+#         return Response(serializer.data)
 
-@api_view(['GET', 'DELETE', 'PUT'])
-def markets_single_view(request, pk):
-    
-    if request.method == 'GET':
-        market = Market.objects.get(pk=pk) 
-        serializer = MarketSerializer(market, many=False, context={'request': request}) 
-        return Response(serializer.data)
-
-    if request.method == 'PUT':
-        market = Market.objects.get(pk=pk)
-        serializer = MarketSerializer(market, data=request.data, partial=True, context={'request': request})
+#     if request.method == 'PUT':
+#         market = Market.objects.get(pk=pk)
+#         serializer = MarketSerializer(market, data=request.data, partial=True, context={'request': request})
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
 
-    if request.method == 'DELETE':
-        market = Market.objects.get(pk=pk) 
-        serializer = MarketSerializer(market, many=False, context={'request': request})
-        market.delete()
-        return Response(serializer.data)
+#     if request.method == 'DELETE':
+#         market = Market.objects.get(pk=pk) 
+#         serializer = MarketSerializer(market, many=False, context={'request': request})
+#         market.delete()
+#         return Response(serializer.data)
 
 
 @api_view(['GET', 'POST'])
@@ -53,6 +65,7 @@ def seller_view(request):
     
     if request.method == 'GET':
         sellers = Seller.objects.all() 
+        # wegen 'many=True' bekommen wir eine Liste mehrerer Objects zurück (ist das eine ListView??)
         serializer = SellerSerializer(sellers, many=True, context={'request': request}) 
         return Response(serializer.data)
 
@@ -75,6 +88,7 @@ def single_seller_view(request, pk):
         return Response({"error": "Seller not found"}, status=404)
 
     if request.method == 'GET':
+        # wegen 'many=False' bekommen wir keine Liste sondern ein einzelnes Object zurück
         serializer = SellerSerializer(seller, many=False,context={'request': request}) 
         return Response(serializer.data)
 
